e56823cad7a0ca6fd5a11fc155a3473be4c181b7
Monique Azevedo <monique.azevedo@original.io>
2019-06-15 20:54:16 -0300

    corrige chamado cors no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use((req, res, next)=>{
   req.io = io
   next()
 })
-app.user(cors())
+app.use(cors())
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
